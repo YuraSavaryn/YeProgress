200c441dbdd6ab3b5e081b77995554d7278a14da
Yura <yura.savaryn.777@gmail.com>
2025-05-14 21:27:54 +0300

    iniliaze backend

---
+++ Backend/src/main/java/com/ccpc/yeprogress/YeProgressApplication.java
@@ -0,0 +1,13 @@
+package com.ccpc.yeprogress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeProgressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeProgressApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/ccpc/yeprogress/YeProgressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccpc.yeprogress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeProgressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
